7aa25c75f28d6dcfd766fea43eb1595ccb8243ad
Sergei Garcia Ortega <sergei.garcia@digitalonus.com>
2019-07-17 15:30:45 -0500

    chore: minor comment typos

--- resolvers.js
+++ resolvers.js
@@ -2,19 +2,22 @@ const axios = require('axios');
 
 module.exports = {
   Query: {
+    // RESTDatasource
     user: async (parent, args, context, info) => {
       const { username } = args;
       const { dataSources } = context;
       // Why is this not working? Redis values stored don't have the correct TTL (Time to Live) set
       /* const { cacheControl } = info;
       cacheControl.setCacheHint({ maxAge: 30 }); */
-      return await dataSources.userAPI.getByUsername({ username }); // Custom DataSource
+      return await dataSources.userAPI.getByUsername({ username });
     },
+    // Custom Datasource
     users: async (parent, args, context, info) => {
       const { usernames } = args;
       const { dataSources } = context;
       return await dataSources.customUserAPI.getManyByUsername({ usernames });
     },
+    // No Datasource
     userRepos: async (parent, args, context, info) => {
       const { username } = args;
       const { data: repos } = await axios.get(`https://api.github.com/users/${username}/repos`);
